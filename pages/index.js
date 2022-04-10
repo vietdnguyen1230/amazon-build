1efcc138bce86cd685619b08844933e2b47b54d6
Viet Nguyen <vietnguyen2330@gmail.com>
2022-04-09 18:57:15 -0700

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,11 @@ import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
 import ProductFeed from "../components/ProductFeed";
+import { getSession, useSession } from "next-auth/client";
 
 export default function Home({ products }) {
+  const [session, loading] = useSession();
+
   return (
     <div className="bg-gray-100 ">
       <Head>
@@ -24,14 +27,14 @@ export default function Home({ products }) {
 }
 
 export async function getServerSideProps(context) {
-  // const session = await getSession(context);
+  const session = await getSession(context);
   const products = await fetch("https://fakestoreapi.com/products").then(
     (res) => res.json()
   );
 
   return {
     props: {
-      // session,
+      session,
       products,
     },
   };
